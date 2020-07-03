33c8c233e56fdc1d1de6961ad9bbf627e0885bb8
Shrish <shrishstark77@gmail.com>
2020-07-03 13:52:09 +0530

    first commit

---
+++ Assets/FIRST.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnButtonClick()
+    {
+        Debug.log("button clicked");
+    }
+}
+}
+}
